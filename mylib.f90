750b058b70dca1566e84ed3520dbf3b3609cf1d1
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2021-09-13 08:33:54 +0200

    make mylib.f90 a module

--- mylib.f90
+++ mylib.f90
@@ -1,4 +1,8 @@
-subroutine hello() bind(C)
-    print *, "Hello from Fortran"
-end subroutine
+module hi
 
+contains
+    subroutine hello() bind(C)
+        print *, "Hello from Fortran"
+    end subroutine
+
+end module hi
